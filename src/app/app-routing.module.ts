1da7ea38ccb10a3e41ba145dbb64b953bbfd694b
Edouard <edouard.nicolas@edu.devinci.fr>
2022-04-30 13:19:32 +0200

    [add]setup styles and cleaning  app.components.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {DefibrillateursComponent} from "./defibrillateurs/defibrillateurs.component";
+import {CommandesComponent} from "./commandes/commandes.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: DefibrillateursComponent},
+  {path: 'commandes', component: CommandesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
