dfd7774508e376ea2b4d4a8691c030b805f451a2
Edouard <edouard.nicolas@edu.devinci.fr>
2022-05-03 22:29:57 +0200

    [add] add filter on dfibrilattors array to only display healthy defibrillators (state == 0)

--- src/app/defibrillateurs-array/defibrillateurs-array.component.ts
+++ src/app/defibrillateurs-array/defibrillateurs-array.component.ts
@@ -10,6 +10,8 @@ export class DefibrillateursArrayComponent implements OnInit {
 
   defibrillators: Defibrillator[] = [];
 
+  filterActive: boolean = false;
+
   constructor(private defibrillatorService: DefibrillatorService ) { }
 
   ngOnInit(){
@@ -26,4 +28,41 @@ export class DefibrillateursArrayComponent implements OnInit {
 
   };
 
-}
+  ngOnChanges(){
+
+
+  }
+
+  filterStatus(status: number){
+    return this.defibrillators.filter(defibrillator => defibrillator.state === status);
+  }
+
+/*toggle this.filterActive*/
+  onFilterClick(){
+    this.filterActive = !this.filterActive;
+
+    /*call the api*/
+    let data: Defibrillator[] = [];
+    return this.defibrillatorService.getAll().subscribe((items: Defibrillator[]) => {
+      data = items;
+      this.defibrillators = [];
+      data.forEach((item :Defibrillator)=>{
+        let defibrillator: Defibrillator;
+        defibrillator = new Defibrillator(item.serial, item.locationName, item.locationAddr, item.state, item.electrodesExpiry);
+        this.defibrillators.push(defibrillator);
+      });
+
+      /*filter result if filter active*/
+      if (this.filterActive) {
+        this.defibrillators = this.filterStatus(0);
+      }
+    });
+
+  };
+
+
+};
+
+
+// Language: typescript
+// Path: src/app/defibrillateurs-array/defibrillateurs-array.component.ts
